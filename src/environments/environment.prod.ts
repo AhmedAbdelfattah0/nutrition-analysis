dd2b4bc43ccb64d8d5951bf4731127ccd4c9f2d4
AhmedAbdelfattah0 <ahmed.m.abdelfttah@gmail.com>
2021-03-25 16:54:18 +0200

    change app details

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   bas_Url:"https://api.edamam.com/api",
-  app_id:"0cb1eb6c",
-  app_key:"6c39799ef615bd504681e4791cbc8793"
+  app_id:"47379841",
+  app_key:"d28718060b8adfd39783ead254df7f92"
 
 };
